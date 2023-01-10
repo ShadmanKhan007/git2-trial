4110f6c9e8ad751cd8266292b86d31a460bcd851
Shadman Khan <shaad2209@gmail.com>
2023-01-10 18:02:49 +0530

    prime number problem is done

---
+++ prime.js
@@ -0,0 +1,20 @@
+let number=3;
+let factor=0;
+
+for(let i=0; i<=number; i++)
+ {
+  if(number%i==0);
+{
+  factor++;
+}
+
+ }
+
+if(factor==2) 
+{
+console.log("yes)
+}
+
+else {
+console.log("No")
+}
