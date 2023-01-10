b0e67472a0944b3be5c562446c9e25bf140a1ba3
Shadman Khan <shaad2209@gmail.com>
2023-01-10 18:22:55 +0530

    pallindrome problem

---
+++ pallindrome.js
@@ -0,0 +1,16 @@
+let str="naman";
+let bag=" "
+
+for(let i=str.length-1; i>=0; i++)
+{
+  bag=bag+str[i]
+}
+
+if(str=bag)
+{
+console.log("pallindrome") 
+}
+
+else {
+console.log("not a pallindrome") 
+}
